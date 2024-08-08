f7113aea35b30adbbaa87e4d9a3a56b1ee3be3a6
mediwateyash <diwateyash2004@gmail.com>
2024-08-09 03:22:56 +0530

    everthing added

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">© 2024 All rights Reserved <a href="https://www.linkedin.com/in/diwateyash2004/" target="_blank" rel="noopener noreferrer">@DiwateYash</a></p>
+</div>
